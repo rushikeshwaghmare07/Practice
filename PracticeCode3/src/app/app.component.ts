44a8e51cabbf038c62460b71a822ad634fc0766c
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-01-19 18:50:17 +0530

    Adding new component

--- PracticeCode3/src/app/app.component.ts
+++ PracticeCode3/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { NewComponent } from './new/new.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FirstComponent, NewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
